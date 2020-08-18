c895a47cdbfdd26c32259e64be2da48399f7a309
dvijay96 <dvijay96@gmail.com>
2020-08-19 00:28:11 +0530

    removed repo

--- src/main/java/com/example/batch/job/processor/UserProcessor.java
+++ src/main/java/com/example/batch/job/processor/UserProcessor.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
-import com.example.batch.job.entity.User;
+import com.example.batch.job.model.User;
 
 @Component
 public class UserProcessor implements ItemProcessor<User, User> {
